1c4083b6956ce0e939363b729a0e09151469711c
alimostafavi24 <119982603+alimostafavi24@users.noreply.github.com>
2023-08-10 18:17:47 -0700

    Add files via upload

---
+++ modules/navier_stokes/include/kernels/SurfaceTension3.h
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include "Kernel.h"
+
+class SurfaceTension3 : public Kernel
+{
+public:
+  SurfaceTension3(const InputParameters & parameters);
+
+  static InputParameters validParams();
+
+protected:
+  virtual Real computeQpResidual() override;
+
+  virtual Real computeQpJacobian() override;
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  const Real _coef;
+
+
+
+  /// Multiplier for the coupled force term
+
+  
+
+  const VariableGradient & _grad_c; // c is the order parameter 
+  const VariableValue & _w;  // w is the chemical potential
+
+
+  unsigned int _component;
+  unsigned int _c_var;
+  unsigned int _w_var;
+
+};
